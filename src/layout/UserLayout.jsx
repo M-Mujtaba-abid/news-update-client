2d6317670067880c229df7723a00aacc0a098d3a
Mujtaba <mujtabaabid06@gmail.com>
2025-08-07 17:05:05 +0500

    fix file name isue

--- src/layout/UserLayout.jsx
+++ src/layout/UserLayout.jsx
@@ -1,40 +1,3 @@
-// import React from 'react'
-// import Navbar from '../shared/Navbar'
-// import Footer from '../shared/Footer'
-// import Home from '../landingpage/Home'
-// import { Route, Routes } from 'react-router-dom'
-// import About from '../landingpage/About'
-// import NewsArticleLayout from '../landingpage/newsArticles/NewsArticleLayout'
-// import SignUp from '../features/Signup'
-// import Login from '../features/Login'
-
-
-// const UserLayout = () => {
-//   return (
-//     <div className='pt-[88px] '>
-      
-//       <Navbar/>
-// {/* <SignUp/> */}
-//       <Routes>
-// <Route path='/' element={<Home/>}/>
-// <Route path='/about' element={<About/>}/>
-// <Route path='/newsarticles' element={<NewsArticleLayout/>}/>
-// <Route path='/signup' element={<SignUp/>}/>
-// <Route path='/login' element={<Login/>}/>
-
-
-//       </Routes>
-
-//       <Footer/>
-      
-//     </div>
-//   )
-// }
-
-// export default UserLayout
-
-
-
 import React from 'react'
 import Navbar from '../shared/Navbar'
 import Footer from '../shared/Footer'
@@ -42,7 +5,7 @@ import Home from '../landingpage/Home'
 import { Route, Routes } from 'react-router-dom'
 import About from '../landingpage/About'
 import NewsArticleLayout from '../landingpage/newsArticles/NewsArticleLayout'
-import SignUp from '../features/Signup'
+import SignUp from '../features/SignUp'
 import Login from '../features/Login'
 // import ProtectedRouting from '../layout/ProtectedRouting'
 
